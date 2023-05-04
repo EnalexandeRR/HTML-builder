359b93f08752244d5c9d918acd4b6f0915af8313
Alexandr Yenussov <a.yenussov@gmail.com>
2023-05-04 20:11:08 +0600

    feat: finish task-03

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const { error } = require('console');
+const path = require('path');
+
+const folderPath = path.join(__dirname, 'secret-folder');
+
+try {
+  ReadDir();
+} catch (err) {
+  console.log(err);
+}
+
+function ReadDir() {
+  fs.promises.readdir(folderPath, { withFileTypes: true }).then((entities) => {
+    const files = entities.filter((element) => element.isFile());
+    files.forEach((file) => {
+      fs.stat(path.join(folderPath, file.name), (error, stats) => {
+        console.log(
+          `${file.name.split('.')[0]} - ${file.name.split('.')[1]} - ${Number(stats.size)}b`,
+        );
+      });
+    });
+  });
+}
