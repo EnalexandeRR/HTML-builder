151f9d1746799efc2e4f239e8a09115e4655b6ef
Alexandr Yenussov <a.yenussov@gmail.com>
2023-05-05 13:50:06 +0600

    feat: finish task-04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const fs = require('fs');
+const path = require('path');
 
+const pathToCopy = path.join(__dirname, 'files-copy');
+
+function copyDir() {
+  fs.mkdir(pathToCopy, { recursive: true }, () => {
+    fs.promises.readdir(pathToCopy).then((files) => {
+      files.forEach((file) => {
+        console.log(file);
+        fs.unlink(path.join(pathToCopy, file), () => {});
+      });
+    });
+    fs.promises.readdir(path.join(__dirname, 'files')).then((files) => {
+      files.forEach((file) => {
+        fs.promises.copyFile(path.join(__dirname, 'files', file), path.join(pathToCopy, file));
+      });
+    });
+  });
+}
+
+copyDir();
