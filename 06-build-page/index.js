b0cd4f9a28714906bb7c23cef079b23c4eea8e84
Alexandr Yenussov <a.yenussov@gmail.com>
2023-05-08 15:11:18 +0600

    feat: finish html & css bundling of task-06

---
+++ 06-build-page/index.js
@@ -0,0 +1,83 @@
+const fs = require('fs');
+const path = require('path');
+
+const pathToFinalDist = path.join(__dirname, 'project-dist');
+const pathToAssets = path.join(__dirname, 'assets');
+
+async function CreateDistFolders() {
+  await fs.promises.rm(pathToFinalDist, { recursive: true, force: true });
+
+  return fs.promises.mkdir(pathToFinalDist, { recursive: true }).then(() => {
+    fs.promises.mkdir(path.join(pathToFinalDist, 'assets'), {
+      recursive: true,
+    });
+  });
+}
+
+// async function CopyAssets(path, parentPath = '') {
+//   const innerData = await fs.promises.readdir(path, { withFileTypes: true });
+
+//   innerData.forEach((fileOrFolder) => {
+//     console.log(fileOrFolder);
+//     if (!fileOrFolder.isFile()) {
+//       // fs.promises.mkdir(path.join(pathToFinalDist, 'assets', fileOrFolder.name), {
+//       //   recursive: true,
+//       // });
+//     }
+//   });
+//   return innerData;
+// }
+
+async function MergeStyles() {
+  const pathToStyles = path.join(__dirname, 'styles');
+  const bundlePath = path.join(__dirname, 'project-dist', 'style.css');
+
+  fs.promises.readdir(pathToStyles, { withFileTypes: true }).then((files) => {
+    const cssFiles = files.filter((file) => file.isFile() && file.name.split('.')[1] === 'css');
+
+    fs.promises.writeFile(bundlePath, '').then((err) => {
+      if (err) throw err;
+      cssFiles.forEach((stylesFile) => {
+        fs.promises
+          .readFile(path.join(pathToStyles, stylesFile.name.toString()), 'utf-8')
+          .then((text) => {
+            fs.appendFile(bundlePath, text, (err) => {
+              if (err) throw err;
+            });
+          });
+      });
+    });
+  });
+}
+
+async function BuildHtml() {
+  const componentsContent = {};
+  // Store component's content in object
+  await fs.promises.readdir(path.join(__dirname, 'components')).then((components) => {
+    components.forEach((component) => {
+      fs.promises
+        .readFile(path.join(__dirname, 'components', component), 'utf8')
+        .then((content) => {
+          componentsContent[component.split('.')[0]] = content;
+        });
+    });
+  });
+
+  const template = await fs.promises.readFile(path.join(__dirname, 'template.html'), 'utf8');
+  let finalHtmlContent = template;
+
+  Object.keys(componentsContent).forEach((key) => {
+    finalHtmlContent = finalHtmlContent.replace(`{{${key}}}`, componentsContent[key]);
+    console.log(finalHtmlContent);
+  });
+  fs.promises.writeFile(path.join(pathToFinalDist, 'index.html'), finalHtmlContent, 'utf8');
+}
+
+async function BuildProject() {
+  await CreateDistFolders();
+  // CopyAssets(pathToAssets);
+  MergeStyles();
+  BuildHtml();
+}
+
+BuildProject();
